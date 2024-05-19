1be99152b5ef8858729a6880c09375ff063b2633
Paolo Coletta <paolocoletta1846@gmail.com>
2024-05-19 19:15:24 +0200

    finish

---
+++ src/main/java/it/pssng/parthIrc/exceptions/IllegalPssngCommandException.java
@@ -0,0 +1,11 @@
+package it.pssng.parthIrc.exceptions;
+
+public class IllegalPssngCommandException extends Exception {
+
+    public IllegalPssngCommandException() {
+        super();
+    }
+    public IllegalPssngCommandException(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/it/pssng/parthIrc/model/SessionData.java
+++ src/main/java/it/pssng/parthIrc/model/SessionData.java
@@ -1,5 +1,6 @@
 package it.pssng.parthIrc.model;
 
+import it.pssng.parthIrc.exceptions.IllegalPssngCommandException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,23 +12,14 @@ public class SessionData {
 
     private String fiscalCode;
     private UserRole role;
-    private String msg;
 
-    public SessionData(String command) {
+    public SessionData(String command) throws IllegalPssngCommandException {
         String[] parts = command.split("\\$\\$");
         if (parts.length >= 3) {
             fiscalCode = parts[0];
             role = Integer.valueOf(parts[1]) == 0 ? UserRole.USER : UserRole.ADMIN;
-            msg = parts[2];
         } else {
-            // Gestione dell'errore o comportamento predefinito in caso di stringa non
-            // valida
-            fiscalCode = "";
-            role = UserRole.USER;
-            msg = "";
-            // Oppure puoi lanciare un'eccezione
-            // throw new IllegalArgumentException("Stringa di comando non valida: " +
-            // command);
+            throw new IllegalPssngCommandException("Illegal command \"" + command + "\"");
         }
     }
 
